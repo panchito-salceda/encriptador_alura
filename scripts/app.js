4b30f4f166e4f212b781918da8a9045ddbe1ae3e
panchito-salceda <fcosalceda@gmail.com>
2024-08-26 16:12:44 -0600

    revision del codigo encriptador

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,34 @@
-var mensaje;
-var mensaje_encriptado;
-var mensaje_temp;
 
-function encriptado(){
-
+const inputText = document.getElementById('inputText')
+const botonTransfer = document.getElementById('btn-encript')
+const showText = document.getElementById('mensaje-encriptado')
+const botonBack = document.getElementById('btn-desencript')
+function generateNumber(){
+    return Math.floor(Math.random() * 10);
 }
 
-function desencriptado(){
-
+function reverseString(mensaje){
+    return mensaje.split("").reverse().join("");
 }
 
-function general_clave(){
+botonTransfer.addEventListener('click', () => {
+        let text = inputText.value;
+        let mensajeCifrado = '';
 
-}
+        for (let i = 0; i< text.length; i++) {
+            mensajeCifrado += text[i];
+            if (i < text.length -1) {
+                mensajeCifrado += generateNumber();
+            }
+        }
+        mensajeInversa = reverseString(mensajeCifrado)
+        showText.textContent = mensajeInversa
+        inputText.value = '';
+    }
+)
+
+botonBack.addEventListener('click', ()=> {
+    let text2 = reverseString(mensajeInversa)
+    alert(text2)
+});
+
